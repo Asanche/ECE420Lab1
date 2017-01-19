ee47211529f814df3c2917b37f768333d4a1bbd4
Niko Somos <nikosomos@uws101806usr.uws.ualberta.ca>
2017-01-19 16:13:10 -0700

    finished

--- demo.c
+++ demo.c
@@ -1,17 +1,27 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "lab1_IO.h"
 #include "timer.h"
 
 
-int** matrix_a, matrix_b;
+int** matrix_a; int** matrix_b; int**matrix_c;
 int size;
 
 void* threadfunc (void* arg_p)
 {
-    
+    long outputSpot = (long) arg_p;
+    long x = outputSpot%size;
+    long y = floor(outputSpot/size);
+    long sum = 0;
+    for (int i = 0; i < size; i++){
+        sum += matrix_a[x][i] * matrix_b[i][y];
+    }
+    matrix_c[x][y] = sum;
+    //printf("sum = %d\n",sum);
+    return NULL;
 } 
 
 int main (int argc, char* argv[])
@@ -21,17 +31,25 @@ int main (int argc, char* argv[])
     pthread_t* thread_handles = malloc(thread_count*sizeof(pthread_t));
 
     Lab1_loadinput(&matrix_a, &matrix_b, &size);
-
-    for(int i = 0; i < size){
+    matrix_c = malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++)
+    {
+      matrix_c[i] = malloc(size * sizeof(int));
+    }
+    double start; double end;
+    GET_TIME(start);
+    for(long i = 0; i < pow(size,2);){
         for(long thread = 0; thread < thread_count; thread++){
-            pthread_create(&thread_handles[thread], NULL, threadfunc, (void*)thread);
-            i++
+            pthread_create(&thread_handles[thread], NULL, threadfunc, (void*)i);
+            i++;
         }
     
         for(long thread = 0; thread < thread_count; thread++){
             pthread_join(thread_handles[thread], NULL);
         }
     }
+    GET_TIME(end);
+    Lab1_saveoutput(matrix_c, &size, end-start);
 }
 
 
