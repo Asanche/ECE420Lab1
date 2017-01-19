eda38e294818b971976881134a4a2b4e6ba94869
asanche <asanche@ualberta.ca>
2017-01-19 15:09:57 -0700

    add stuff ...

---
+++ demo.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lab1_IO.h"
+#include "timer.h"
+
+
+int** matrix_a, matrix_b;
+int size;
+
+void* threadfunc (void* arg_p)
+{
+    
+} 
+
+int main (int argc, char* argv[])
+{
+    //todo: error handling for input
+    int thread_count = strtol(argv[1], NULL, 10);
+    pthread_t* thread_handles = malloc(thread_count*sizeof(pthread_t));
+
+    Lab1_loadinput(&matrix_a, &matrix_b, &size);
+
+    for(long thread = 0; thread < thread_count; thread++){
+        pthread_create(&thread_handles[thread], NULL, threadfunc, (void*)thread);
+    }
+    
+    
+    for(long thread = 0; thread < thread_count; thread++){
+        pthread_join(thread_handles[thread], NULL);
+    }
+}
+
+
+
